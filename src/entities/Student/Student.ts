a10e008bc09dc803273a3d54a392218950005009
Matheus Campos <devcodecampos@gmail.com>
2023-08-13 21:22:55 -0300

    feat: add Student entity

---
+++ src/entities/Student/Student.ts
@@ -0,0 +1,31 @@
+import { User, UserProps } from "../User/User";
+
+interface StudentProps {
+  studentClass: string;
+  registrationId: string;
+}
+
+export class Student extends User {
+  private _studentProps: StudentProps;
+
+  constructor(userProps: UserProps, studentProps: StudentProps) {
+    super(userProps);
+    this._studentProps = studentProps;
+  }
+
+  get studentClass(): string {
+    return this._studentProps.studentClass;
+  }
+
+  set studentClass(studentClass: string) {
+    this._studentProps.studentClass = studentClass;
+  }
+
+  get registrationId(): string {
+    return this._studentProps.registrationId;
+  }
+
+  set registrationId(registrationId: string) {
+    this._studentProps.registrationId = registrationId;
+  }
+}
