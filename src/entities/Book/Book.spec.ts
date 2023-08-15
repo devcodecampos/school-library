70d4cafb75a6ed0fa7ec3f721a33a92bbb3d2c4d
codecampos <devcodecampos@gmail.com>
2023-08-15 14:53:25 -0300

    test: add Book test

---
+++ src/entities/Book/Book.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "vitest";
+import { Book } from "./Book";
+
+test("create an book", () => {
+  const publishDate = new Date("2017-09-27");
+
+  const book = new Book({
+    id: "BA-012",
+    title: "The Things You Can See Only When You Slow Down",
+    author: "Haemin Sunim",
+    publishDate: publishDate,
+    genre: "self-help",
+    quantityAvailable: 2,
+  });
+
+  expect(book).toBeInstanceOf(Book);
+  expect(book.id).toEqual("BA-012");
+  expect(book.title).toEqual("The Things You Can See Only When You Slow Down");
+  expect(book.author).toEqual("Haemin Sunim");
+  expect(book.publishDate).toEqual(publishDate);
+  expect(book.genre).toEqual("self-help");
+  expect(book.quantityAvailable).toEqual(2);
+});
